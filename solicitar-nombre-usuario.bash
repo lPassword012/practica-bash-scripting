0445ecbf8138cc3ffc1076f7fe8494dbe0d1f087
lPassword012 <m2035ausv@gmail.com>
2025-05-14 17:17:19 +0000

    add: agregado script de solicitud nombre y edad

---
+++ solicitar-nombre-usuario.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Ingrese sus datos para registrase"
+
+echo "Ingrese su nombre"
+read nombre
+echo "Ingrese su edad"
+read edad
+echo "Hola $nombre, tienes $edad, registro exitoso."
+echo "Bienvenido al sistema $nombre"
